b3b471a361651b59d6f9b3d19dc68b42f89035c4
Cody Condon <codycondon50@gmail.com>
2017-08-19 12:49:30 -0400

    initial setup and integrate firebase

---
+++ findSocialApp/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  findSocialApp
+//
+//  Created by Cody Condon on 2017-08-19.
+//  Copyright © 2017 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
